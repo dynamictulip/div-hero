7484659a55445ba24937f52d85205ea0eab8622e
Sara Gowen <me@dynamictulip.com>
2020-05-01 15:41:39 +0100

    moved move logic to GamePiece

--- ts/Hero.ts
+++ ts/Hero.ts
@@ -1,60 +1,40 @@
-class Hero {
-    private hero: JQuery<HTMLElement>;
-    private currentVerticalSpeed: number = 0;
-    private currentHorizontalSpeed: number = 0;
+class Hero extends GamePiece {
 
-    constructor(playAreaId: string) {
-        this.hero = $("#" + playAreaId).add("div")
-        this.hero.addClass("hero");
+    protected piece: JQuery<HTMLElement>;
 
-        this.addEventHandlers();
+    public constructor(playAreaId: string) {
+        super();
+        this.piece = ($("#" + playAreaId).add("div").addClass("hero"));
 
-        window.setInterval(this.moveHero, 20, this)
+        this.addMotionSensors();
+
+        window.setInterval(this.moveHero, 10, this)
     }
 
-    private addEventHandlers() {
+    private addMotionSensors() {
         $(document).keydown(this.handleKeyDown);
         $(document).keyup(this.handleKeyUp);
     }
 
-    public updatePosition() {
-        let currentPosition = this.hero.offset();
-        const correction = -5;
-
-        if (currentPosition) {
-            if (this.currentVerticalSpeed != 0)
-                this.hero.css("top", currentPosition.top + this.currentVerticalSpeed + correction);
-            if (this.currentHorizontalSpeed != 0)
-                this.hero.css("left", currentPosition.left + this.currentHorizontalSpeed + correction);
-        }
-    }
-
-    private moveHero(givenHero: Hero) {
-        givenHero.updatePosition();
-    }
-
     private handleKeyDown = (event: JQuery.KeyDownEvent<Document, null, Document, Document>) => {
-        let currentPosition = this.hero.offset();
-
-        if (currentPosition)
-            switch (event.key) {
-                case "ArrowDown": {
-                    this.currentVerticalSpeed = 3;
-                    break;
-                }
-                case "ArrowUp": {
-                    this.currentVerticalSpeed = -3;
-                    break;
-                }
-                case "ArrowLeft": {
-                    this.currentHorizontalSpeed = -3
-                    break;
-                }
-                case "ArrowRight": {
-                    this.currentHorizontalSpeed = 3
-                    break;
-                }
+        switch (event.key) {
+            case "ArrowDown": {
+                this.currentVerticalSpeed = this.speed;
+                break;
             }
+            case "ArrowUp": {
+                this.currentVerticalSpeed = -this.speed;
+                break;
+            }
+            case "ArrowLeft": {
+                this.currentHorizontalSpeed = -this.speed
+                break;
+            }
+            case "ArrowRight": {
+                this.currentHorizontalSpeed = this.speed
+                break;
+            }
+        }
     }
 
     private handleKeyUp = (event: JQuery.KeyUpEvent<Document, null, Document, Document>) => {
